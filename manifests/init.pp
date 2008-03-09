ed454644f3551069d10497523ae511c41b98d970
mh <mh@immerda.ch>
2008-03-09 21:15:20 +0000

    added dcc module

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,9 +1,48 @@
-# modules/skeleton/manifests/init.pp - manage skeleton stuff
+# modules/exim/manifests/init.pp - manage exim stuff
 # Copyright (C) 2007 admin@immerda.ch
 #
 
-# modules_dir { "skeleton": }
+# modules_dir { "exim": }
 
-class skeleton {
+class exim {
+    case $operatingsystem {
+        gentoo: { include exim::gentoo }
+        default: { include exim::base }
+    }
+}
 
+class exim::base {
+    package{'exim':
+        ensure => installed,
+    }
+
+    service{exim:
+        ensure => running,
+        enable => true,
+        #hasstatus => true, #fixme!
+        require => Package[exim],
+    }
+
+}
+
+class exim::gentoo inherits exim::base {
+    Package[exim]{
+        category => 'mail-mta',
+    }
+
+    #conf.d file if needed
+    Service[exim]{
+       require +> File["/etc/conf.d/exim"],
+    }
+    file { "/etc/conf.d/exim":
+        owner => "root",
+        group => "0",
+        mode  => 644,
+        ensure => present,
+        source => [
+            "puppet://$server/dist/exim/conf.d/${fqdn}/exim",
+            "puppet://$server/dist/exim/conf.d/exim",
+            "puppet://$server/exim/conf.d/exim"
+        ]
+    }
 }
