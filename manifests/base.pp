e71fdae5b8e0d37c8f89dbdbf6621b7e3c286e59
mh <mh@immerda.ch>
2009-05-19 22:56:47 +0200

    refactored everything in it's own file, deploy exim config

---
+++ manifests/base.pp
@@ -0,0 +1,25 @@
+class exim::base {
+    package{'exim':
+        ensure => installed,
+    }
+
+    service{exim:
+        ensure => running,
+        enable => true,
+        hasstatus => true,
+        require => Package[exim],
+    }
+
+    file{'/etc/exim/exim.conf':
+        source => [ "puppet://$server/files/exim/${fqdn}/exim.conf",
+                    "puppet://$server/files/exim/exim.conf",
+                    "puppet://$server/exim/exim.conf" ],
+        require => Package['exim'],
+        notify => Service['exim'],
+        owner => root, group => mail, mode => 0640;
+    }
+
+    if $use_munin {
+        include exim::munin
+    }
+}
---
+++ manifests/gentoo.pp
@@ -0,0 +1,21 @@
+class exim::gentoo inherits exim::base {
+    Package[exim]{
+        category => 'mail-mta',
+    }
+
+    #conf.d file if needed
+    Service[exim]{
+       require +> File["/etc/conf.d/exim"],
+    }
+    file { "/etc/conf.d/exim":
+        owner => "root",
+        group => "0",
+        mode  => 644,
+        ensure => present,
+        source => [
+            "puppet://$server/files/exim/conf.d/${fqdn}/exim",
+            "puppet://$server/files/exim/conf.d/exim",
+            "puppet://$server/exim/conf.d/exim"
+        ]
+    }
+}
