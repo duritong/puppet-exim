01c47c413538338a4e6db2e63488affb043b7b7a
mh <mh@immerda.ch>
2009-05-23 03:36:50 +0200

    added exim plugins

--- manifests/munin.pp
+++ manifests/munin.pp
@@ -1,1 +1,15 @@
-class exim::munin {}
+class exim::munin {
+  file{'/var/lib/munin/plugin-state/exim_mailstats':
+    ensure => present,
+    replace => false,
+    require => Package['exim'],
+    owner => nobody, group => exim, mode => 0640;
+  }
+
+  munin::plugin{
+    'exim_mailstats':
+      config => 'group exim';
+    'exim_mailqueue':
+      config => 'group exim';
+  }
+}
--- manifests/munin/disable.pp
+++ manifests/munin/disable.pp
@@ -1,1 +1,9 @@
-class exim::munin::disable inherits exim::munin {}
+class exim::munin::disable inherits exim::munin {
+  File{'/var/lib/munin/plugin-state/exim_mailstats']{
+    ensure => absent,
+  }
+
+  Munin::Plugin['exim_mailstats', 'exim_mailqueue']{
+      ensure => 'absent',
+  }
+}
