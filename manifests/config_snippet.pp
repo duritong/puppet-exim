dcc0bee1a7386acc37064e4e4b1b0ba4aaeb3c03
mh <mh@immerda.ch>
2009-12-07 00:14:40 +0100

    introduce exim component cluster var

--- manifests/config_snippet.pp
+++ manifests/config_snippet.pp
@@ -1,6 +1,7 @@
 define exim::config_snippet(){
   file{"/etc/exim/conf.d/${name}":
-    source => [ "puppet://$server/modules/site-exim/${fqdn}/conf.d/${name}",
+    source => [ "puppet://$server/modules/site-exim/conf.d/${fqdn}/${name}",
+                "puppet://$server/modules/site-exim/conf.d/${exim_component_cluster}/${name}",
                 "puppet://$server/modules/site-exim/conf.d/${name}" ],
     require => Packge['exim'],
     notify => Service['exim'],
