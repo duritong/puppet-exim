88a5f6631315697c7cdfe6dde5b5635e278cef7b
mh <mh@immerda.ch>
2009-12-07 21:01:20 +0100

    introduce content for config_snippets

--- manifests/config_snippet.pp
+++ manifests/config_snippet.pp
@@ -1,10 +1,18 @@
-define exim::config_snippet(){
+define exim::config_snippet($content = 'absent'){
   file{"/etc/exim/conf.d/${name}":
-    source => [ "puppet://$server/modules/site-exim/conf.d/${fqdn}/${name}",
-                "puppet://$server/modules/site-exim/conf.d/${exim_component_cluster}/${name}",
-                "puppet://$server/modules/site-exim/conf.d/${name}" ],
     require => Packge['exim'],
     notify => Service['exim'],
     owner => root, group => mail, mode => 0640;
+  }
+  if ($content=='absent'){
+    File["/etc/exim/conf.d/${name}"]{
+      source => [ "puppet://$server/modules/site-exim/conf.d/${fqdn}/${name}",
+                "puppet://$server/modules/site-exim/conf.d/${exim_component_cluster}/${name}",
+                "puppet://$server/modules/site-exim/conf.d/${name}" ],
+    }
+  } else {
+    File["/etc/exim/conf.d/${name}"]{
+      content => $content,
+    }
   }
 }
