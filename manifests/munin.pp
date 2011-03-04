ed3c1a377e926a00252f713474e4c66732778e2f
mh <mh@immerda.ch>
2011-03-04 19:10:18 +0100

    debianize some last parts

--- manifests/munin.pp
+++ manifests/munin.pp
@@ -1,14 +1,18 @@
 class exim::munin {
+  $group = $operatingsystem ? {
+    'debian' => 'Debian-exim',
+    default => 'exim'
+  }
   file{'/var/lib/munin/plugin-state/exim_mailstats':
     ensure => present,
     replace => false,
     require => Package['exim'],
-    owner => nobody, group => exim, mode => 0640;
+    owner => nobody, group => $group, mode => 0640;
   }
 
   munin::plugin{
     'exim_mailstats':
-      config => 'group exim';
+      config => "group ${group}";
     'exim_mailqueue':
       config => "env.exim /usr/sbin/exim
 group exim";
