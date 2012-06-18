66fb9cd2eb249181cc01b09a4f01ecef3b2bdb3e
mh <mh@immerda.ch>
2012-06-18 15:32:57 -0300

    we don't need to remove the munin stuff when disabling

--- manifests/disable.pp
+++ manifests/disable.pp
@@ -2,7 +2,4 @@ class exim::disable {
   case $::kernel {
     linux: { include exim::disable::base }
   }
-  if $exim::manage_munin {
-    include ::exim::munin::disable
-  }
 }
--- manifests/munin/disable.pp
+++
@@ -1,9 +0,0 @@
-class exim::munin::disable inherits exim::munin {
-  File['/var/lib/munin/plugin-state/exim_mailstats']{
-    ensure => absent,
-  }
-
-  Munin::Plugin['exim_mailstats', 'exim_mailqueue']{
-    ensure => 'absent',
-  }
-}
