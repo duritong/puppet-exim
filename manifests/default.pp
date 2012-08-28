740c3082fe3e27c5df0ff84eef29103e5a8f6b3c
mh <mh@immerda.ch>
2012-08-28 08:07:43 +0200

    only do that on systems that require it to do

--- manifests/default.pp
+++ manifests/default.pp
@@ -1,6 +1,5 @@
 class exim::default {
   case $::operatingsystem {
-    centos: {  include exim::default::centos }
-    default: { fail("not yet able to set exim as default mta on ${::operatingsystem}") }
+    centos,redhat,fedora: {  include exim::default::centos }
   }
 }
