c156df94e47318afbb7e9a4ac7018f9be8ef46b1
mh <mh@immerda.ch>
2012-12-13 14:39:44 +0100

    fix dependency and dependency

--- manifests/munin.pp
+++ manifests/munin.pp
@@ -1,33 +1,36 @@
+# setup munin monitoring for exim
 class exim::munin {
   $group = $::operatingsystem ? {
-    'debian' => 'Debian-exim',
-    default => 'exim'
+    'debian'  => 'Debian-exim',
+    default   => 'exim'
   }
   file{'/var/lib/munin/plugin-state/exim_mailstats':
-    ensure => present,
+    ensure  => present,
     replace => false,
-    require => Package['exim'],
-    owner => nobody, group => $group, mode => 0640;
+    require => Package['exim','munin-node'],
+    owner   => nobody,
+    group   => $group,
+    mode    => '0640';
   }
   $logdir = $::operatingsystem ? {
-    'debian' => '/var/log/exim4',
-    default => '/var/log/exim'
+    'debian'  => '/var/log/exim4',
+    default   => '/var/log/exim'
   }
   $logfile = $::operatingsystem ? {
-    'debian' => 'mainlog',
-    default => 'main.log'
+    'debian'  => 'mainlog',
+    default   => 'main.log'
   }
   $stats_group = $::operatingsystem ? {
-    'debian' => 'adm',
-    default => 'exim'
+    'debian'  => 'adm',
+    default   => 'exim'
   }
   munin::plugin{
     'exim_mailstats':
-      config => "env.logdir ${logdir}
+      config  => "env.logdir ${logdir}
 env.logname ${logfile}
 group ${stats_group}";
     'exim_mailqueue':
-      config => "env.exim /usr/sbin/exim
+      config  => "env.exim /usr/sbin/exim
 group ${group}";
   }
 }
